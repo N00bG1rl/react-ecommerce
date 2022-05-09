45c5d5fe25360cd7f4a2f1e07f8121669ff29245
N00bG1rl <403helen@gmail.com>
2022-05-09 21:53:08 +0300

    Add products to firebase db

--- src/components/products/AddProductForm.js
+++ src/components/products/AddProductForm.js
@@ -1,5 +1,50 @@
-function AddProductForm() {
-	return <div>AddProductForm</div>
+import { useRef } from 'react'
+
+import Card from '../ui/Card'
+import styles from './AddProductForm.module.css'
+
+function AddProductForm({ onAddProduct }) {
+	const imageInputRef = useRef()
+	const nameInputRef = useRef()
+	const priceInputRef = useRef()
+
+	function submitHandler(event) {
+		event.preventDefault()
+
+		const enteredImage = imageInputRef.current.value
+		const enteredName = nameInputRef.current.value
+		const enteredPrice = priceInputRef.current.value
+
+		const productData = {
+			image: enteredImage,
+			name: enteredName,
+			price: enteredPrice,
+		}
+
+		onAddProduct(productData)
+	}
+
+	return (
+		<Card>
+			<form className={styles.form} onSubmit={submitHandler}>
+				<p className={styles.control}>
+					<label htmlFor='image'>Product image</label>
+					<input type='file' required id='image' ref={imageInputRef} />
+				</p>
+				<p className={styles.control}>
+					<label htmlFor='name'>Product name</label>
+					<input type='text' required id='name' ref={nameInputRef} />
+				</p>
+				<p className={styles.control}>
+					<label htmlFor='price'>Product price</label>
+					<input type='number' required id='price' ref={priceInputRef} />
+				</p>
+				<button type='submit' className={styles.actions}>
+					Add Product
+				</button>
+			</form>
+		</Card>
+	)
 }
 
 export default AddProductForm
--- src/pages/NewProduct.js
+++ src/pages/NewProduct.js
@@ -1,5 +1,35 @@
+import { useNavigate } from 'react-router-dom'
+
+import AddProductForm from '../components/products/AddProductForm'
+
+//const URL = process.env.REACT_APP_FIREBASE_API
+
 function NewProduct() {
-	return <div>NewProduct</div>
+	const navigate = useNavigate()
+
+	const handleProductAdd = productData => {
+		console.log(productData)
+
+		fetch(
+			'https://react-ecommerce-4c48d-default-rtdb.europe-west1.firebasedatabase.app/products.json',
+			{
+				method: 'POST',
+				body: JSON.stringify(productData),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		).then(() => {
+			navigate('/')
+		})
+	}
+
+	return (
+		<section>
+			<h1>Add new product</h1>
+			<AddProductForm onAddProduct={handleProductAdd} />
+		</section>
+	)
 }
 
 export default NewProduct
